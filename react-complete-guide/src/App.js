f5584ffe95725c2ddc0f61e909e42ac727711455
Duwon Ha <bruceha9898@gmail.com>
2022-11-30 00:06:34 +1000

    learnt props & composition (children )

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,33 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      title: "Toyota",
+      price: 100,
+      date: new Date(2022, 19.04),
+    },
+    {
+      title: "Honda",
+      price: 123,
+      date: new Date(2020, 22.2),
+    },
+    {
+      title: "KIA",
+      price: 14,
+      date: new Date(2019, 3.4),
+    },
+    {
+      title: "Porche",
+      price: 1,
+      date: new Date(2022, 31.12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
