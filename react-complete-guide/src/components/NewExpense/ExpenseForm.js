fba3c546fc3132423542cbf61c5382468f2782eb
Duwon Ha <bruceha9898@gmail.com>
2022-12-01 19:22:11 +1000

    Passing data from bottom to top (using customised props with function)

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { isValidElement, useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -47,7 +47,10 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -55,13 +58,18 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
             onChange={AmountChangeHandler}
+            value={enteredAmount}
             min="0.01"
             step="0.01"
           />
@@ -71,6 +79,7 @@ const ExpenseForm = () => {
           <input
             type="date"
             onChange={DateChangeHandler}
+            value={enteredDate}
             min="2019-01-01"
             max="2022-12-31"
           />
